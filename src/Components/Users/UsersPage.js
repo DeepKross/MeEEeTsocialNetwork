f027c0deab940bbae4851e1204e4c1533a16213d
Mike Tanchuk <volkermischa@gmail.com>
2023-02-21 19:07:20 +0200

    Users functional component was converted into class component

--- src/Components/Users/UsersPage.js
+++ src/Components/Users/UsersPage.js
@@ -1,27 +1,30 @@
 import User from "./User/User";
 import axios from "axios";
+import React from "react";
+
 const url4Avatar = "https://graphicriver.img.customer.envatousercontent.com/files/383753376/Letter+A+Logo+-+Anglex_1.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=590&s=1322843b6a4dc44c34d61c195d61ee3e";
 
-function Users(props){
-    debugger;
-    let users = props.users.map(e => <User key={e.id} id={e.id} name={e.name} status={e.status}
-                                           ava={url4Avatar} followedStatus={e.followed}
-                                           followUser={props.followUser} unfollowUser={props.unFollowUser} ></User>);
-    debugger;
-    if(props.users.length === 0)
-    {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
+            this.props.setUsers(response.data.items);
         })
     }
 
+    render() {
 
+        let users = this.props.users.map(e => <User key={e.id} id={e.id} name={e.name} status={e.status}
+                                                    ava={url4Avatar} followedStatus={e.followed}
+                                                    followUser={this.props.followUser}
+                                                    unfollowUser={this.props.unFollowUser}></User>);
 
-    return (
-        <div>
+        return <div>
             Users will be here
             {users}
         </div>
-    );
+    }
 }
+
 export default Users;
